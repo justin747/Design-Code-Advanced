6981f4b6541607b2c4a98b115a9e5b83eefab9d5
Justin747 <justin747@gmail.com>
2021-11-12 16:56:38 -0800

    ProfileView Complete
    ProfileView Complete with Name, Info about user, Social Media links, and a button to go to the previous screen

--- Design+Code Advanced/Views/ProfileView.swift
+++ Design+Code Advanced/Views/ProfileView.swift
@@ -38,12 +38,92 @@ struct ProfileView: View {
                             Text("View Profile")
                                 .foregroundColor(Color.white.opacity(0.7))
                                 .font(.footnote)
-                            
                         }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            print("Segue to settings")
+                        }, label: {
+                            TextFieldIcon(iconName: "gearshape.fill", currentlyEditing: .constant(true))
+                        })
+                    }
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(Color.white.opacity(0.1))
+                    
+                    Text("Best In The World")
+                        .foregroundColor(Color.white)
+                        .font(.title2.bold())
+                    
+                    Label("Former WWE Champ. Future AEW Champ", systemImage: "calendar")
+                        .foregroundColor(Color.white.opacity(0.7))
+                        .font(.footnote)
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(Color.white.opacity(0.1))
+                    
+                    HStack(spacing: 16) {
+                        Image("Twitter")
+                            .resizable()
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .frame(width: 24, height: 24)
+                        
+                        Image(systemName: "link")
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .font(.system(size: 17, weight: .semibold, design: .rounded))
+                        
+                        Text("designcode.io")
+                            .foregroundColor(Color.white.opacity(0.7))
+                            .font(.footnote)
                     }
                 }
+                .padding(16)
+                
+                GradientButton(buttonTitle: "Purchase AEW Premium") {
+                    print("IAP")
+                }
+                .padding(.horizontal, 16)
+                
+                Button(action: {
+                    print("Restore")
+                }, label: {
+                    GradientText(text: "Restore Purchases")
+                        .font(.footnote.bold())
+                })
+                    .padding(.bottom)
             }
+            .background(
+                RoundedRectangle(cornerRadius: 30)
+                    .stroke(Color.white.opacity(0.2))
+                    .background(Color("secondaryBackground").opacity(0.5))
+                    .background(VisualEffectBlur(blurStyle: .dark))
+                    .shadow(color: Color("shadowColor").opacity(0.5), radius: 60, x: 0, y: 30)
+            )
+            .cornerRadius(30)
+            .padding(.horizontal)
+            
+            VStack {
+                Spacer()
+                Button(action: {
+                    print("Sign Out")
+                }, label: {
+                    Image(systemName: "arrow.turn.up.forward.iphone.fill")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 15, weight: .medium, design: .rounded))
+                        .rotation3DEffect(Angle(degrees: 180), axis: (x: 0.0, y: 0.0, z: 1.0))
+                        .background(Circle()
+                                        .stroke(Color.white.opacity(0.2), lineWidth: 1)
+                                        .frame(width: 42, height: 42, alignment: .center)
+                                        .overlay(VisualEffectBlur(blurStyle: .dark).cornerRadius(21)
+                                                    .frame(width: 42, height: 42, alignment: .center)))
+                })
+            }
+            .padding(.bottom, 64)
         }
+        .colorScheme(.dark)
     }
 }
 
