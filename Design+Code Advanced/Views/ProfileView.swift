778c17a54a732715a51a6d2790cb00a22cec96a4
Justin747 <justin747@gmail.com>
2021-11-12 16:11:49 -0800

    Started ProfileView build
    Started ProfileView build with a circle avatar and username within a ZStack

--- Design+Code Advanced/Views/ProfileView.swift
+++ Design+Code Advanced/Views/ProfileView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile View")
+        ZStack {
+            Image("background-2")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack(spacing: 16) {
+                        ZStack {
+                            Circle()
+                                .foregroundColor(Color("pink-gradient-1"))
+                                .frame(width: 66, height: 66, alignment: .center)
+                            
+                            Image(systemName: "person.fill")
+                                .foregroundColor(Color.white)
+                                .font(.system(size: 24, weight: .medium, design: .rounded))
+                        }
+                        .frame(width: 66, height: 66, alignment: .center)
+                        
+                        VStack(alignment: .leading) {
+                            Text("CM Punk")
+                                .foregroundColor(Color.white)
+                                .font(.title2)
+                                .bold()
+                            
+                            Text("View Profile")
+                                .foregroundColor(Color.white.opacity(0.7))
+                                .font(.footnote)
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
