1cc9480e4c67804b357e9f2685215129dabfda51
Justin747 <justin747@gmail.com>
2021-11-14 19:55:57 -0800

    Added gradient effect to profile picture
    Added circular gradient effect to profile picture and added animation when profile picture appears.

--- Design+Code Advanced/Views/GradientProfilePictureView.swift
+++ Design+Code Advanced/Views/GradientProfilePictureView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct GradientProfilePictureView: View {
+    
+    @State private var angle = 0.0
+    
+    var gradient1: [Color] = [
+        Color.init(red: 101/255, green: 134/255, blue: 1),
+        Color.init(red: 1, green: 64/255, blue: 80/255),
+        Color.init(red: 109/255, green: 1, blue: 185/255),
+        Color.init(red: 39/255, green: 232/255, blue: 1)
+    ]
+    
+    var profilePicture: UIImage
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: gradient1), center: .center, angle: .degrees(angle))
+                .mask(Circle()
+                        .frame(width: 70, height: 70, alignment: .center)
+                        .blur(radius: 8.0))
+                .blur(radius: 8.0)
+                .onAppear() {
+                    withAnimation(.linear(duration: 7)) {
+                        self.angle += 360
+                    }
+                }
+            
+            Image(uiImage: profilePicture)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 66, height: 66, alignment: .center)
+                .mask(Circle())
+        }
     }
 }
 
 struct GradientProfilePictureView_Previews: PreviewProvider {
     static var previews: some View {
-        GradientProfilePictureView()
+        GradientProfilePictureView(profilePicture: UIImage(named: "Profile")!)
     }
 }
