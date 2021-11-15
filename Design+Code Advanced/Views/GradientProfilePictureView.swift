24b61133e6574ec58584a95b1a65a314008e862a
Justin747 <justin747@gmail.com>
2021-11-14 19:14:10 -0800

    Created GradientProfilePictureView
    Created GradientProfilePictureView

---
+++ Design+Code Advanced/Views/GradientProfilePictureView.swift
@@ -0,0 +1,20 @@
+//
+//  GradientProfilePictureView.swift
+//  Design+Code Advanced
+//
+//  Created by Justin747 on 11/14/21.
+//
+
+import SwiftUI
+
+struct GradientProfilePictureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GradientProfilePictureView_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientProfilePictureView()
+    }
+}
